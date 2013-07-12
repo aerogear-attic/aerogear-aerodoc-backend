531146b0796febe96dd7fdd5851a12585038c937
Sebastien Blanc <scm.blanc@gmail.com>
2013-07-12 14:03:43 +0200

    name refactoring, change sender version

--- src/main/java/org/jboss/aerogear/aerodoc/service/LeadSender.java
+++ src/main/java/org/jboss/aerogear/aerodoc/service/LeadSender.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.jboss.aerogear.unifiedpush.DefaultJavaSender;
+import org.jboss.aerogear.unifiedpush.async.AsyncClient;
 import org.jboss.aerogear.unifiedpush.resteasy.RestEasyClient;
 import org.jboss.aerogear.aerodoc.model.Lead;
 
@@ -29,14 +30,21 @@ public class LeadSender {
 
 	private String serverURL = "http://localhost:8080/ag-push";
 
-	private String pushApplicationId = "317cce6d-efaf-4ff8-94e3-a45c0a3a688c";
+	private String pushApplicationId = "c7fc6525-5506-4ca9-9cf1-55cc261ddb9c";
+	
+	private String masterPassWord = "8b2f43a9-23c8-44fe-bee9-d6b0af9e316b";
 	
 	//TODO we don't this
 	private int leadVersion = 1;
 	private int broadcastVersion = 1;
 
-	private DefaultJavaSender defaultJavaSender = new DefaultJavaSender(
-			serverURL, new RestEasyClient());
+	
+	public LeadSender() {
+		defaultJavaSender =  new DefaultJavaSender(
+				serverURL, new AsyncClient());
+	}
+
+	private DefaultJavaSender defaultJavaSender;
 
 	public void sendLeads(List<String> users, Lead lead) {
 
@@ -52,7 +60,7 @@ public class LeadSender {
 		json.put("sound" ,"default");
 		json.put("alert" ,"A new lead has been created");
 
-		defaultJavaSender.sendTo(users, json, pushApplicationId);
+		defaultJavaSender.sendTo(users, json, pushApplicationId,masterPassWord);
 	}
 
 	public void sendBroadCast(Lead lead) {
@@ -67,7 +75,7 @@ public class LeadSender {
 		json.put("simple-push", categories);
 		json.put("alert" ,"A new lead has been accepted");
 		json.put("sound" ,"default");
-		defaultJavaSender.broadcast(json, pushApplicationId);
+		defaultJavaSender.broadcast(json, pushApplicationId,masterPassWord);
 	}
 
 	public String getServerURL() {
