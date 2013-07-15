d75ec7ce206b821267ce33cdbfe385683346cd61
Sebastien Blanc <scm.blanc@gmail.com>
2013-07-15 11:21:57 +0200

    angular module rename

--- src/main/webapp/client/scripts/filters.js
+++ src/main/webapp/client/scripts/filters.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('prodoctor.filters',[]).filter('startFrom', function() {
+angular.module('aerodoc.filters',[]).filter('startFrom', function() {
 return function(input, start) {
 start = +start; //parse to int
 return input.slice(start);
