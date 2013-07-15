31a0f7fdc5eeb999e2bf5fe96f92b1ff3e180c98
Sebastien Blanc <scm.blanc@gmail.com>
2013-07-15 18:40:21 +0200

    nuke SPS client

--- src/main/webapp/client/scripts/filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('aerodoc.filters',[]).filter('startFrom', function() {
-return function(input, start) {
-start = +start; //parse to int
-return input.slice(start);
-};
-});
