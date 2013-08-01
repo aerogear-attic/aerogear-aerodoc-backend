9d88c5e79a5d8b3084eee0264f57e89935cb22c6
Sebastien Blanc <scm.blanc@gmail.com>
2013-08-01 11:16:15 +0200

    removed some ugly log statement

--- src/main/java/org/jboss/aerogear/aerodoc/rest/AerodocBaseEndpoint.java
+++ src/main/java/org/jboss/aerogear/aerodoc/rest/AerodocBaseEndpoint.java
@@ -25,49 +25,48 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 /**
- * The pupose of this class is to offer CORS support. All the OPTIONS
- * requests, no matter the path, will be handled in this class. It also offers
- * convenient method to append the allow origin header to any request.
- * Any Endpoint class that would like CORS support should extends this class.
+ * The pupose of this class is to offer CORS support. All the OPTIONS requests,
+ * no matter the path, will be handled in this class. It also offers convenient
+ * method to append the allow origin header to any request. Any Endpoint class
+ * that would like CORS support should extends this class.
  */
 @Path("{path : .*}")
 public abstract class AerodocBaseEndpoint {
 
-    /**
-     * All the OPTIONS requests will hit this method, the wildcard path defined
-     * on class level make it useless to define different path patterns.
-     */
-    @OPTIONS
-    public Response crossOriginForInstallations(@Context HttpHeaders headers) {
-        System.out.println("IN OPTIONS");
-        return appendPreflightResponseHeaders(headers, Response.ok()).build();
-    }
+	/**
+	 * All the OPTIONS requests will hit this method, the wildcard path defined
+	 * on class level make it useless to define different path patterns.
+	 */
+	@OPTIONS
+	public Response crossOriginForInstallations(@Context HttpHeaders headers) {
+		return appendPreflightResponseHeaders(headers, Response.ok()).build();
+	}
 
-    private ResponseBuilder appendPreflightResponseHeaders(HttpHeaders headers,
-            ResponseBuilder response) {
-        // add response headers for the preflight request
-        // required
-        response.header("Access-Control-Allow-Origin",
-                headers.getRequestHeader("Origin").get(0))
-                .header("Access-Control-Allow-Methods",
-                        "POST, DELETE, GET, PUT")
-                .header("Access-Control-Allow-Headers",
-                        "accept, origin, content-type, authorization")
-                .header("Access-Control-Allow-Credentials", "true");
+	private ResponseBuilder appendPreflightResponseHeaders(HttpHeaders headers,
+			ResponseBuilder response) {
+		// add response headers for the preflight request
+		// required
+		response.header("Access-Control-Allow-Origin",
+				headers.getRequestHeader("Origin").get(0))
+				.header("Access-Control-Allow-Methods",
+						"POST, DELETE, GET, PUT")
+				.header("Access-Control-Allow-Headers",
+						"accept, origin, content-type, authorization")
+				.header("Access-Control-Allow-Credentials", "true");
 
-        return response;
-    }
+		return response;
+	}
 
-    /**
-     * This convenient method will append to the response headers the needed
-     * CORS headers
-     */
-    protected Response appendAllowOriginHeader(ResponseBuilder rb,
-            HttpServletRequest request) {
+	/**
+	 * This convenient method will append to the response headers the needed
+	 * CORS headers
+	 */
+	protected Response appendAllowOriginHeader(ResponseBuilder rb,
+			HttpServletRequest request) {
 
-        return rb
-                .header("Access-Control-Allow-Origin",
-                        request.getHeader("Origin")) // return submitted origin
-                .header("Access-Control-Allow-Credentials", "true").build();
-    }
+		return rb
+				.header("Access-Control-Allow-Origin",
+						request.getHeader("Origin")) // return submitted origin
+				.header("Access-Control-Allow-Credentials", "true").build();
+	}
 }
