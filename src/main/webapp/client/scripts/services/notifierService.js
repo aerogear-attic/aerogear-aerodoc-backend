216ad1e4d2a6ff5ef2d93deafd933d0356db833a
Matthias Wessendorf <matzew@apache.org>
2013-08-15 17:46:54 +0200

    Update usage code and submit the 'simplePushEndpoint' on first registration

--- src/main/webapp/client/scripts/services/notifierService.js
+++ src/main/webapp/client/scripts/services/notifierService.js
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,28 +25,42 @@ aerodoc.factory("notifierService", function() {
     broadcastRequest = navigator.push.register();
     broadcastRequest.onsuccess = function (event) {
       broadcastEndpoint = event.target.result;
-      var broadCastMetadata = {
-        deviceToken: broadcastEndpoint.channelID,
-        category: "broadcast"
-      }
 
-      UPClient.registerWithPushServer(broadCastMetadata);
-      console.log("Subscribed to Broadcast messages on " + broadcastEndpoint.channelID);
-      console.log(localStorage.getItem(broadcastEndpoint.channelID) || 1);
-     };
+      // first registration?
+      if ( broadcastEndpoint.pushEndpoint ) {
+        var broadCastMetadata = {
+          deviceToken: broadcastEndpoint.channelID,
+          category: "broadcast",
+          simplePushEndpoint: broadcastEndpoint.pushEndpoint
+        }
+
+        UPClient.registerWithPushServer(broadCastMetadata);
+        console.log("Subscribed to Broadcast messages on " + broadcastEndpoint.channelID);
+        console.log(localStorage.getItem(broadcastEndpoint.channelID) || 1);
+      } else {
+        console.log("Already registered");
+      }
+    };
 
     leadRequest = navigator.push.register();
     leadRequest.onsuccess = function (event) {
       leadEndpoint = event.target.result;
-      var leadMetadata = {
-        deviceToken: leadEndpoint.channelID,
-        alias: sessionStorage.getItem("username"),
-        category: "lead"
-      }
 
-      UPClient.registerWithPushServer(leadMetadata);
-      console.log("Subscribed to lead messages on " + leadEndpoint.channelID);
-      console.log(localStorage.getItem(leadEndpoint.channelID) || 1);
+      // first registration?
+      if ( broadcastEndpoint.pushEndpoint ) {
+        var leadMetadata = {
+          deviceToken: leadEndpoint.channelID,
+          alias: sessionStorage.getItem("username"),
+          category: "lead",
+          simplePushEndpoint: broadcastEndpoint.pushEndpoint
+        }
+
+        UPClient.registerWithPushServer(leadMetadata);
+        console.log("Subscribed to lead messages on " + leadEndpoint.channelID);
+        console.log(localStorage.getItem(leadEndpoint.channelID) || 1);
+      } else {
+        console.log("Already registered");
+      }
      };
     }
   };
